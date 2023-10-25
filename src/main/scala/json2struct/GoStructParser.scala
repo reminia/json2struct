6de40b614f0742d9a8550478fcda3a395f072e6b
sleefd <sleefd@gmail.com>
2023-10-25 22:21:55 +0800

    parse seq of structs together

--- src/main/scala/json2struct/GoStructParser.scala
+++ src/main/scala/json2struct/GoStructParser.scala
@@ -1,6 +1,7 @@
 package json2struct
 
-import json2struct.GoStructAST.{Field, Tag, Struct => AStruct}
+import json2struct.GoStructAST.{Field, Struct, Tag}
+import json2struct.Printer.Syntax.toPrinterOps
 
 import scala.collection.mutable
 import scala.util.parsing.combinator.JavaTokenParsers
@@ -46,12 +47,14 @@ object GoStructParser extends JavaTokenParsers {
       Field.Simple(name, tpe, t.fold[Tag](Tag.None)(identity))
   } | array
 
-  lazy val struct: Parser[AStruct] = ("type" ~> ident ~ "struct" ~ "{" ~ field.* <~ "}") ^^ {
-    case name ~ _ ~ _ ~ seq => AStruct(name, seq)
+  lazy val struct: Parser[Struct] = ("type" ~> ident ~ "struct" ~ "{" ~ field.* <~ "}") ^^ {
+    case name ~ _ ~ _ ~ seq => Struct(name, seq)
   }
 
-  def parse(input: String): Option[AStruct] = {
-    parseAll(struct, input) match {
+  lazy val structs: Parser[Seq[Struct]] = struct.*
+
+  def parse(input: String): Option[Seq[Struct]] = {
+    parseAll(structs, input) match {
       case Success(res, _) => Some(res)
       case NoSuccess(msg, _) =>
         println(msg)
@@ -60,17 +63,27 @@ object GoStructParser extends JavaTokenParsers {
   }
 
   def main(args: Array[String]): Unit = {
-    println(parse(
+    val option = parse(
       """
         |type OpenAiResponse struct {
-        |    Model string     `json:"model,omitempty" xml:"model"`
-        |    Choices []Choices     `json:"choices"`
-        |    Usage Usage     `json:"usage"`
-        |    Object string     `json:"object"`
-        |    Id string     `json:"id"`
-        |    Created int     `json:"created"`
+        |	Id      string   `json:"id"`
+        |	Object  string   `json:"object"`
+        |	Created uint64   `json:"created"`
+        |	Model   string   `json:"model"`
+        |	Choices []Choice `json:"choices"`
+        |	Usage   Usage    `json:"usage"`
         |}
-        |""".stripMargin))
+        |
+        |type Usage struct {
+        |    Completion_tokens int     `json:"completion_tokens"`
+        |    Prompt_tokens int     `json:"prompt_tokens"`
+        |    Total_tokens int     `json:"total_tokens"`
+        |}
+        |
+        |""".stripMargin)
+    option.foreach(seq =>
+      seq.foreach(s => println(s.print()))
+    )
   }
 
 }
