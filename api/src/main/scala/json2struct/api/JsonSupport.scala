1131124e867672310c3a6309761bd9ce1d7772ba
sleefd <sleefd@gmail.com>
2023-11-05 12:45:16 +0800

    finish api

--- api/src/main/scala/json2struct/api/JsonSupport.scala
+++ api/src/main/scala/json2struct/api/JsonSupport.scala
@@ -6,11 +6,8 @@ import spray.json.{DefaultJsonProtocol, RootJsonFormat}
 
 trait JsonSupport extends SprayJsonSupport with DefaultJsonProtocol {
   implicit val jsonFormat: RootJsonFormat[Json] = jsonFormat2(Json.apply)
-  implicit val structFormat: RootJsonFormat[Struct] = jsonFormat1(Struct.apply)
 }
 
 object JsonSupport {
   final case class Json(name: String, json: String)
-
-  final case class Struct(struct: String)
 }
--- api/src/main/scala/json2struct/api/Server.scala
+++ api/src/main/scala/json2struct/api/Server.scala
@@ -7,7 +7,7 @@ import akka.http.scaladsl.server.Directives
 import json2struct.Converter
 import json2struct.Printer.Syntax.toPrinterOps
 import json2struct.api.Conf.{APP_CONF, HttpPort}
-import json2struct.api.JsonSupport.{Json, Struct}
+import json2struct.api.JsonSupport.Json
 
 object Server extends Directives with JsonSupport {
 
@@ -15,23 +15,23 @@ object Server extends Directives with JsonSupport {
     implicit val system = ActorSystem("server", APP_CONF)
 
     val route = concat(
-      path("v1/convert/json") {
+      path("v1" / "convert" / "json") {
         post {
           entity(as[Json]) { json =>
             complete {
               Converter.convertJson(json.json, json.name)
                 .map(_.print())
-                .mkString("\n")
+                .mkString(System.lineSeparator())
             }
           }
         }
       },
 
-      path("v1/convert/struct") {
+      path("v1" / "convert" / "struct") {
         post {
-          entity(as[Struct]) { struct =>
+          entity(as[String]) { struct =>
             complete {
-              Converter.convertStruct(struct.struct).map(_.print())
+              Converter.convertStruct(struct).map(_.print()).mkString(System.lineSeparator())
             }
           }
         }
