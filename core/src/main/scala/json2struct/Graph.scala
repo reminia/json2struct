98a06203249cf083fedc680daefe6cca2f483b7b
sleefd <sleefd@gmail.com>
2023-11-14 23:37:42 +0800

    remove useless outdegree method

--- core/src/main/scala/json2struct/Graph.scala
+++ core/src/main/scala/json2struct/Graph.scala
@@ -7,8 +7,6 @@ import json2struct.GoType.GoArray
 case class Graph[A](nodes: Set[A], edges: Set[(A, A)]) {
   def indegree(a: A): Int = edges.count(_._2 == a)
 
-  def outdegree(a: A): Int = edges.count(_._1 == a)
-
   def sources: Set[A] = nodes.filter(x => indegree(x) == 0)
 }
 
